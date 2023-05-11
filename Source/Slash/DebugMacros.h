0efc247688a1620d97742a9c1c283e014f18adcd
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-05-10 17:08:01 -0700

    Initial Commit

---
+++ Source/Slash/DebugMacros.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "DrawDebugHelpers.h"
+#include "GraphColor/Private/appconst.h"
+
+//Macros
+#define DRAW_SPHERE(Location) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Blue, true);
+#define DRAW_LINE(StartLocation, EndLocation) if(GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Cyan, true, -1.f, 0, 1);
+#define DRAW_POINT(Location) if(GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Cyan, true);
+#define DRAW_VECTOR(StartLocation, EndLocation) if(GetWorld()) \
+	{ \
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Cyan, true, -1.f, 0, 1); \
+		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Cyan, true); \
+	}
+#define DRAW_SPHERE_COLOR(Location, HexColor) if(GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::FromHex(FString(HexColor)), true);
+#define DRAW_BOX(Location) if(GetWorld()) \
+	{ \
+		DrawDebugBox(GetWorld(), Location, FVector(10,10,10), FColor::Red, true , -1.f, 0, 4.f); \
+	}
---
+++ Source/Slash/Private/Items/Item.cpp
@@ -0,0 +1,37 @@
+#include "Items/Item.h"
+#include "DrawDebugHelpers.h"
+#include "Slash/DebugMacros.h"
+
+
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+	FString HexColor = TEXT("#e3f542");
+	//DRAW_SPHERE(Location);
+	//DrawDebugSphere(World, Location, 30.f, 24, FColor::FromHex(FString(HexColor)), true );
+	//DRAW_SPHERE_COLOR(Location, HexColor);
+	/*DRAW_LINE(Location, Location + Forward * 100);
+	DRAW_POINT(Location + Forward * 100);*/
+	//DRAW_VECTOR(Location, Location + Forward * 100);
+	//DrawDebugBox(World, Location, FVector(10,10,10), FColor::FromHex(FString(HexColor)), true, -1.f, 0, 4.f );
+	DRAW_BOX(Location);
+		
+	
+}
+
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
