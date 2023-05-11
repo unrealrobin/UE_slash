e3d81c0d65b390a72ac48aaa9c71c76f47896fb7
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-05-10 17:31:57 -0700

    Quick Change for Commented Code

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -23,9 +23,9 @@ void AItem::BeginPlay()
 	//DRAW_SPHERE_COLOR(Location, HexColor);
 	/*DRAW_LINE(Location, Location + Forward * 100);
 	DRAW_POINT(Location + Forward * 100);*/
-	//DRAW_VECTOR(Location, Location + Forward * 100);
+	DRAW_VECTOR(Location, Location + Forward * 100);
 	//DrawDebugBox(World, Location, FVector(10,10,10), FColor::FromHex(FString(HexColor)), true, -1.f, 0, 4.f );
-	DRAW_BOX(Location);
+	//DRAW_BOX(Location);
 		
 	
 }
