d9a61475ce53773db25ac47cb6acefed86ddfc39
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-05-11 21:47:41 -0700

    Created and Used UPROPERTY() Macros and the Sin FMath Function

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -20,13 +20,13 @@ void AItem::BeginPlay()
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	RunningTime += DeltaTime;
 
-	//Movement Rate in Units of CM/Sec
-	float MovementRate = 50.f;
-	float RotationRate = 45.f;
-	//MovementRate * DeltaTime (cm/s * s/frame) = cm/frame
-	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
-	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+
+	AddActorWorldOffset(FVector(0.f,0.f, DeltaZ));
+	
 	DRAW_SPHERE_SINGLE_FRAME(GetActorLocation());
 	DRAW_VECTOR_SINGLE_FRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 	
