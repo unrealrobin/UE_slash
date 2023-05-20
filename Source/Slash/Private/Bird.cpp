201da76b5d5323737d1ee6520e4f3e427484879e
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-05-20 11:53:33 -0700

    Added Movement to Pawn and Ability to Look Around using Enhanced Input

--- Source/Slash/Private/Bird.cpp
+++ Source/Slash/Private/Bird.cpp
@@ -4,26 +4,67 @@
 #include "Bird.h"
 
 #include "Components/CapsuleComponent.h"
-#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ABird::ABird()
 {
  	
 	PrimaryActorTick.bCanEverTick = true;
-	UCapsuleComponent* BirdCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
-	BirdCapsule->SetCapsuleRadius(15.f);
-	BirdCapsule->SetCapsuleHalfHeight(20.f);
-	SetRootComponent(BirdCapsule);
-	USkeletalMeshComponent* BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdSkeletalMeshComponent"));
-	BirdMesh -> SetupAttachment(GetRootComponent());
+	
+	TheCapsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	TheCapsule->SetCapsuleRadius(15.f);
+	TheCapsule->SetCapsuleHalfHeight(20.f);
+	SetRootComponent(TheCapsule);
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdSkeletalMeshComponent"));
+	Mesh->SetupAttachment(GetRootComponent());
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 50.f;
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	
 }
 
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
+
 	
+	if(const APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if(Subsystem)
+		{
+			//Applying our specific BirdMappingContext to this Subsystem
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	}
+}
+
+void ABird::Move(const FInputActionValue& Value)
+{
+	const float DirectionValue = Value.Get<float>();
+	if(DirectionValue && (DirectionValue != 0.f))
+	{
+		const FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, DirectionValue);
+		
+	}
+}
+
+void ABird::Look(const FInputActionValue& Value)
+{
+	const FVector2d CurrentValue = Value.Get<FVector2d>();
+	if(CurrentValue.X && CurrentValue.Y)
+	{
+		AddControllerPitchInput(CurrentValue.Y);
+		AddControllerYawInput(CurrentValue.X);
+	}
 }
 
 
@@ -33,10 +74,16 @@ void ABird::Tick(float DeltaTime)
 
 }
 
-
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//Binding Enhanced Input Action
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABird::Look);
+	};
 
 }
 
--- Source/Slash/Public/Bird.h
+++ Source/Slash/Public/Bird.h
@@ -3,11 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Pawn.h"
 #include "Bird.generated.h"
 
+//Forward Declared Classes, so .h Files can be included in .cpp
 class UCapsuleComponent;
 class USkeletalMeshComponent;
+class UInputAction;
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class SLASH_API ABird : public APawn
@@ -25,14 +30,30 @@ protected:
 	
 	virtual void BeginPlay() override;
 
+	//Define a Single Context for our Input Map
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	class UInputMappingContext* BirdMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputAction* LookAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
 	
-
 private:
+	
 	UPROPERTY(VisibleAnywhere)
-	UCapsuleComponent* Capsule;
-
+	UCapsuleComponent* TheCapsule;
+	
 	UPROPERTY(VisibleAnywhere)
 	USkeletalMeshComponent* Mesh;
-	
 
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
 };
