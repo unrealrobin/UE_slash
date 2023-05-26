24c4988edaefb18d422bdf5a67f621eb8e770e2d
Robin Lifshitz <Rlifshitz19@gmail.com>
2023-05-25 20:54:40 -0700

    Added Some Enhanced Input for Character Forward movement

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -1,34 +1,45 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Characters/SlashEcho.h"
+#include "Characters\SlashCharacter.h"
 
-// Sets default values
+
 ASlashEcho::ASlashEcho()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
+
 void ASlashEcho::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+void ASlashEcho::Move(FInputActionValue& Value)
+{
+	const float DirectionValue = Value.Get<float>();
+	if(DirectionValue && (DirectionValue != 0.f))
+	{
+		const FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, DirectionValue);
+		
+	}
+}
+
+
 void ASlashEcho::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
+
 void ASlashEcho::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	
 }
 
--- Source/Slash/Public/Characters/SlashCharacter.h
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
-#include "SlashEcho.generated.h"
+#include "SlashCharacter.generated.h"
 
 UCLASS()
 class SLASH_API ASlashEcho : public ACharacter
@@ -12,18 +13,12 @@ class SLASH_API ASlashEcho : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ASlashEcho();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	virtual void BeginPlay() override;
+	
+	void Move(FInputActionValue& Value);
 };
